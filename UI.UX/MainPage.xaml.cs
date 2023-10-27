d73737e6ade53edd963cf801378f5a9d4533ad62
Myakish <65065415+Myakish-dev@users.noreply.github.com>
2023-10-27 16:12:14 +0300

    переход на окно авторизации

--- UI.UX/MainPage.xaml.cs
+++ UI.UX/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace UI.UX
+﻿using Microsoft.Maui;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace UI.UX
 {
     public partial class MainPage : ContentPage
     {
@@ -7,10 +10,27 @@
         public MainPage()
         {
             InitializeComponent();
+             //< Entry
+             //       Text = "{Binding Text}"
+             //       Placeholder = "Логин"
+             //   />
+
+             //   < Entry
+             //       Placeholder = "Пароль"
+             //       IsPassword = "True"
+             //       Text = "{Binding Password}"
+
+             //   />
+                //Text text = new Text
+                //{
+                //    text = "логин",
+                //    HorizontalOptions = LayoutOptions.Center,
+                //    VerticalOptions = LayoutOptions.Center
+                //}
 
             Button button = new Button
             {
-                Text = "Navigate!",
+                Text = "Войти",
                 HorizontalOptions = LayoutOptions.Center,
                 VerticalOptions = LayoutOptions.Center
             };
